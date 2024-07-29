52c5c17b768c3ae76f8918da4ce252389509928c
Hyemin Yang <asitwas729@gmail.com>
2024-07-29 20:43:08 +0900

    2-2
    연산을 Calculator클래스에서 수행함.

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -5,10 +5,9 @@ import java.util.Scanner;
 
 public class App {
   public static void main(String[] args) {
+    // Calculator 인스턴스 생성
+    Calculator calculator = new Calculator();
     Scanner sc = new Scanner(System.in);
-    int firstNumber = 0;
-    int secondNumber = 0;
-    char FBO;   // 사칙연산(Four Basic Operations)
     int result = 0;   // 계산 결과
     String answer = ""; //의사를 물어보는 변수
     //int[] resultArray = new int[3]; // 연산결과 10개를 저장할 수 있는 배열 선언
@@ -16,13 +15,13 @@ public class App {
     ArrayList<Integer> resultList = new ArrayList<Integer>();
     while( !answer.equals("exit") ){
       System.out.print("첫 번째 숫자를 입력하세요: ");
-      firstNumber = sc.nextInt();
+      int num1 = sc.nextInt();
       System.out.print("두 번째 숫자를 입력하세요: ");
-      secondNumber = sc.nextInt();
-      System.out.println(firstNumber + " " + secondNumber);
+      int num2 = sc.nextInt();
+      System.out.println("입력한 값: "+num1 + " " + num2);
       System.out.print("사칙연산 기호를 입력하세요: ");
-      FBO = sc.next().charAt(0);
-      System.out.println(FBO);
+      char operator = sc.next().charAt(0);
+      System.out.println(operator);
 
 
   //1-6 주석처리
@@ -132,51 +131,35 @@ public class App {
 //          System.out.println("사칙연산 부호를 잘못 입력하셨습니다.");
 //        }
  //     }
-      if((firstNumber >= 0) && (secondNumber >= 0)){
-        if(FBO == '+'){
-          result = firstNumber + secondNumber;
-          System.out.println("결과: "+result);
-          resultList.add(result);
-        } else if(FBO == '-'){
-          result = firstNumber - secondNumber;
-          resultList.add(result);
-        } else if(FBO == '*'){
-          result = firstNumber * secondNumber;
-          resultList.add(result);
-        } else if(FBO == '/'){
-            if(!(secondNumber == 0)){
-              result = firstNumber / secondNumber;
-              resultList.add(result);
-            }else {
-              System.out.println("나눗셈 연산에서 0이 입력될 수 없습니다.");
-            }
-        }
+      if((num1 >= 0) && (num2 >= 0)){
+        result = calculator.Calculate(num1, num2, operator);
+//        if(FBO == '+'){
+//          result = firstNumber + secondNumber;
+//          System.out.println("결과: "+result);
+//          resultList.add(result);
+//        } else if(FBO == '-'){
+//          result = firstNumber - secondNumber;
+//          resultList.add(result);
+//        } else if(FBO == '*'){
+//          result = firstNumber * secondNumber;
+//          resultList.add(result);
+//        } else if(FBO == '/'){
+//            if(!(secondNumber == 0)){
+//              result = firstNumber / secondNumber;
+//              resultList.add(result);
+//            }else {
+//              System.out.println("나눗셈 연산에서 0이 입력될 수 없습니다.");
+//            }
+//        }
       } else{
         System.out.println("양의 정수를 입력해주세요.");
       }
       System.out.print("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제) ");
-      answer = sc.next();
-      if(answer.equals("remove")){
-        resultList.remove(0);
-      }
+      calculator.answer(sc.next());
       System.out.print("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회) ");
-      answer = sc.next();
-      if(answer.equals("inquiry")){
-        for(int inquiry : resultList){
-          System.out.print(inquiry + " ");
-        }
-        //
-      }
+      calculator.answer(sc.next());
       System.out.print("더 계산하시겠습니까? (exit 입력 시 종료) : ");
-      answer = sc.next();
+      calculator.answer(sc.next());
     }
-      System.out.println("종료 됩니다.");
-      System.exit(0);   // 자바 시스템 종료 구문
-
-
-
-
-
-
   }
 }
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,17 +1,25 @@
 package calculator;
 
+import java.util.ArrayList;
+
 public class Calculator {
+  ArrayList<Integer> resultList = new ArrayList<Integer>();
   public int Calculate(int a, int b, char c){
     int result = 0;
     if(c == '+'){
       result = a + b;
+      resultList.add(result);
+      System.out.println(resultList.toString());
     } else if(c == '-'){
       result = a - b;
+      resultList.add(result);
     } else if(c == '*'){
       result = a * b;
+      resultList.add(result);
     } else if(c == '/'){
       if(b != 0){
         result = a / b;
+        resultList.add(result);
       } else {
         System.out.println("나눗셈에서 분모에 0을 넣을수는 없습니다.");
       }
@@ -19,5 +27,18 @@ public class Calculator {
       System.out.println("사칙연산기호를 잘못입력하셨습니다.");
     }
     return result;
+  }
+  public void answer(String answer){
+    System.out.println("입력값 : "+answer);
+    if(answer.equals("remove")){
+      resultList.remove(0);
+    } else if(answer.equals("inquiry")){
+      for(int inquiry : resultList){
+        System.out.print(inquiry + " ");
+      }
+    } else if(answer.equals("exit")){
+      System.out.println("종료 됩니다.");
+      System.exit(0);   // 자바 시스템 종료 구문
+    }
   }
 }
